7d9a702b597f5f825de3a5355cdd5796c271f549
galihpermana29 <galihcbn123@gmail.com>
2025-03-20 00:36:54 +0700

    feat: initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  distDir: process.env.NODE_ENV === "production" ? "build" : ".next",
+  // distDir: process.env.NODE_ENV === "production" ? "build" : ".next",
   images: {
     unoptimized: true,
     domains: [
